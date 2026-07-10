003c7b0c274bbc7007a15b348f4431244cdb8580
thaysss <emanuellythays15@gmail.com>
2026-07-10 12:51:13 -0300

    separar ambientes de desenvolvimento

--- smtt_backend/app/config.py
+++ smtt_backend/app/config.py
@@ -6,13 +6,26 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    """Configurações base da aplicação."""
+    """Configurações base comuns a todos os ambientes."""
     SECRET_KEY = os.getenv('SECRET_KEY', 'chave-padrao-de-seguranca')
-    
-    # Configuração do Banco de Dados (Usaremos o SQLAlchemy padrão do Flask)
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ENGINE_OPTIONS = {
-    "pool_pre_ping": True,
-    "pool_recycle": 300,
+        "pool_pre_ping": True,
+        "pool_recycle": 300,
     }
+
+class DevelopmentConfig(Config):
+    """Configurações específicas para o ambiente de desenvolvimento local."""
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+
+class ProductionConfig(Config):
+    """Configurações específicas para o ambiente de produção."""
+    DEBUG = False
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+
+class TestingConfig(Config):
+    """Configurações específicas para execução de testes unitários."""
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
