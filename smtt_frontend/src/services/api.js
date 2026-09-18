75194d3683115cd05830b21d693bc2a11dbb431f
thaysss <emanuellythays15@gmail.com>
2026-09-17 23:10:42 -0300

    correção noticias

--- smtt_frontend/src/services/api.js
+++ smtt_frontend/src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const defaultApiUrl = import.meta.env.PROD
-  ? 'https://site-smtt-production-b2fc.up.railway.app/api'
+  ? '/api'
   : 'http://localhost:5000/api';
 
 const api = axios.create({
